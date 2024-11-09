96519f1e7f462ceefe5bfbbc2dcfaa6b1c74cf8f
Daria Milyakova <103563978+GithubAgitator@users.noreply.github.com>
2024-11-09 22:15:36 +0300

    tasks2/3

--- task2.py
+++ task2.py
@@ -9,3 +9,34 @@
 # который добавляет информацию о количестве дверей.
 # Класс Motorcycle должен иметь поле has_sidecar (True или False)
 # и метод get_info, который указывает, есть ли у мотоцикла коляска.
+
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_info(self):
+        return self.make, self.model, self.year
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, number_of_doors):
+        super().__init__(make, model, year)
+        self.number_of_doors = number_of_doors
+
+    def get_info(self):
+        return f'{self.make} {self.model} год выпуска {self.year} колличество дверей {self.number_of_doors}'
+
+class Motorcycle(Vehicle):
+    def __init__(self, make, model, year, has_sidecar=True):
+        super().__init__(make, model, year)
+        self.has_sidecar = has_sidecar
+
+    def get_info(self):
+        return f'{self.make} {self.model} год выпуска {self.year} {self.has_sidecar}'
+
+car = Car("Китай", "BMV", 2020, 5)
+print(car.get_info())
+
+motocikl = Motorcycle("Япония", "Enduro", 2021, False)
+print(motocikl.get_info())
--- task3.py
+++ task3.py
@@ -18,3 +18,55 @@
 
 # Метод add_device(device), который добавляет устройство в магазин.
 # Метод list_devices(), который выводит информацию обо всех устройствах.
+
+class Device:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def get_info(self):
+        return self.brand, self.model, self.price
+
+class Laptop(Device):
+    def __init__(self, brand, model, price, ram, storage):
+        super().__init__(brand, model, price)
+        self.ram = ram
+        self.storage = storage
+
+    def get_info(self):
+        return self.brand, self.model, self.price, self.ram, self.storage
+
+class Smartphone(Device):
+    def __init__(self, brand, model, price, camera_megapixels, battery_capacity):
+        super().__init__(brand, model, price)
+        self.camera_megapixels = camera_megapixels
+        self.battery_capacity = battery_capacity
+
+    def get_info(self):
+        return self.brand, self.model, self.price, self.camera_megapixels, self.battery_capacity
+
+class Store:
+    def __init__(self):
+        self.lst = []
+    def add_device(self, device):
+        self.lst.append(device)
+
+    def list_devices(self):
+        for i in self.lst:
+            print(list(i.get_info()))
+
+laptop = Laptop("Lenovo", "V-777", 25000, "4gb", "S2200" )
+laptop2 = Laptop("Asus", "V-117", 55000, "3gb", "S2700" )
+print(laptop.get_info())
+new_latop = Store()
+new_latop.add_device(laptop)
+new_latop.add_device(laptop2)
+new_latop.list_devices()
+
+smartphone1 = Smartphone('Androud', "Nokia", "G22", 10, 3000)
+smartphone2 = Smartphone('Androud', "Realme", "10", 11, 3700)
+new_latop2 = Store()
+new_latop2.add_device(smartphone1)
+new_latop2.add_device(smartphone2)
+new_latop2.list_devices()
